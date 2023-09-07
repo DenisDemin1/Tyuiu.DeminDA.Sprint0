bb3edd1ff951b23b5617505f792d1c9f119a1443
DeminDenis1 <maxwelldenis888@gmail.com>
2023-09-07 17:10:56 +0500

    Task2

---
+++ Tyuiu.DeminDA.Sprint0.Task2.V0.Test/DataSeviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DeminDA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.DeminDA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Денис";
+            var res = DataService.GetMessage(name);
+            //Вызывается класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Денис", res);
+        }
+    }
+}
---
+++ Tyuiu.DeminDA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DeminDA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DeminDA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58c9d47f-3033-4a9a-afc2-9b466cf96740")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
